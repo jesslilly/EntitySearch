d7b2cc095f6de7199b31d8bc46a7d6978d27bfcb
jlilly <jlilly@veic.org>
2016-03-31 22:47:15 -0400

    Beginning of the searchPopup

---
+++ EntitySearch/EntitySearch.Demo.Web/Scripts/entity-search.js
@@ -0,0 +1,45 @@
+﻿
+// TODO It might be nice to allow users to create their own template for the drop down?
+//<template class="hello-template">
+//  <div class="hello">
+//    Hello, world!
+//  </div>
+//</template>
+//<script>
+//  var helloTemplate = document.querySelector('.hello-template');
+//  var helloDiv = document.importNode(helloTemplate.content, true);
+//  document.querySelector('body').appendChild(helloDiv);
+//</script>
+
+(function (document) {
+
+    console.log("entity-search loaded.");
+
+    function showSearchPopup() {
+        console.log("showSearchPopup on " + this.id);
+        var newDiv = document.createElement("div");
+        newDiv.id = 'entity-search-popup'; // TODO come up with a better way.
+        var newContent = document.createTextNode("Recently Viewed Pages");
+        newDiv.appendChild(newContent);
+        this.parentElement.appendChild(newDiv);
+    }
+
+    function hideSearchPopup() {
+        console.log("hideSearchPopup");
+        var searchPopup = document.getElementById("entity-search-popup");
+        searchPopup.parentElement.removeChild(searchPopup);
+    }
+
+    function pageLoad() {
+        var searchBoxes = document.querySelectorAll('[data-entity-search]');
+        // http://stackoverflow.com/questions/13433799/why-doesnt-nodelist-have-foreach
+        for (var index = 0; index < searchBoxes.length; index++) {
+            var searchBox = searchBoxes[index];
+            searchBox.addEventListener("focus", showSearchPopup);
+            searchBox.addEventListener("blur", hideSearchPopup);
+        }
+    }
+
+    pageLoad();
+
+})(document);
